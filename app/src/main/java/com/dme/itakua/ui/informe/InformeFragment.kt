0f68d2efb5301f34c98ce6255bf9503d76394865
EnriMelga <enriquemelga270499@gmail.com>
2022-06-13 19:06:54 -0400

    Commit para merge

--- app/src/main/java/com/dme/itakua/ui/informe/InformeFragment.kt
+++ app/src/main/java/com/dme/itakua/ui/informe/InformeFragment.kt
@@ -1,4 +1,4 @@
-package com.dme.itakua.ui.slideshow
+package com.dme.itakua.ui.informe
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dme.itakua.databinding.FragmentSlideshowBinding
+import com.dme.itakua.databinding.FragmentInformeBinding
 
-class SlideshowFragment : Fragment() {
+class InformeFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentInformeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,16 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val informeViewModel =
+            ViewModelProvider(this).get(InformeViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentInformeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        /*val textView: TextView = binding.textSlideshow
+        informeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
--- app/src/main/java/com/dme/itakua/ui/informe/InformeViewModel.kt
+++ app/src/main/java/com/dme/itakua/ui/informe/InformeViewModel.kt
@@ -1,13 +1,13 @@
-package com.dme.itakua.ui.slideshow
+package com.dme.itakua.ui.informe
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class InformeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is informe Fragment"
     }
     val text: LiveData<String> = _text
 }
